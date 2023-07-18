1b258af1ab5b57790706c15d3f567778d4d5df7b
Mohit Manna <mohit.manna-e@maf.ae>
2023-07-18 22:23:45 +0530

    manager-with-at-least-5-ref

---
+++ Spark/manager-with-at-least-5-ref.py
@@ -0,0 +1,26 @@
+import findspark
+findspark.init()
+import pyspark
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.window import Window
+spark = SparkSession.builder.master("local[*]").appName("Manna").getOrCreate()
+
+# https://leetcode.com/problems/managers-with-at-least-5-direct-reports
+
+df=spark.read.csv("SampleData/manager-with-at-least-5-ref.csv",header=True)
+df.createOrReplaceTempView("employee")
+spark.sql("""
+with x as(
+select managerId as id
+from Employee
+group by managerId 
+having count(*)>=5
+)
+select name from Employee where id in (select id from x)
+""").show()
+mdf=df.groupBy("managerId").count().filter("count >= 5").select("managerId")
+df.join(mdf,df.id==mdf.managerId).select("name").show()
+#close spark
+spark.sparkContext._gateway.close()
+spark.stop()
